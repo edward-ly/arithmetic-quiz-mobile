ec6e0f1901664a929f0f5a6530f888e3f7262564
Edward Ly <edvard.ly@gmail.com>
2018-01-21 12:37:48 -0800

    Moved main app view to src/js folder

--- App.js
+++ App.js
@@ -1,23 +1,5 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Entry point for create-react-native-app
+import React from 'react';
+import Application from './src/js/Application';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App () { return <Application />; }
