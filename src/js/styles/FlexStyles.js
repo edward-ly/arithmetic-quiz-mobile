96d4ce37dac1f141df60fbb7d37f9232fa7f8c66
Edward Ly <edvard.ly@gmail.com>
2018-01-24 14:12:31 -0800

    Added more flex style classes

--- src/js/styles/FlexStyles.js
+++ src/js/styles/FlexStyles.js
@@ -1,16 +1,41 @@
 import { StyleSheet } from "react-native";
 
 export default FlexStyles = StyleSheet.create({
+  // declare flex box
   flex: {
     flex: 1,
   },
+  // flex-direction
   flexRow: {
     flexDirection: "row",
+  },
+  // align-items
+  alignStart: {
+    alignItems: "flex-start",
   },
   alignCenter: {
     alignItems: "center",
   },
+  alignEnd: {
+    alignItems: "flex-end",
+  },
+  alignStretch: {
+    alignItems: "stretch",
+  },
+  // justify-content
+  justifyStart: {
+    justifyContent: "flex-start",
+  },
   justifyCenter: {
     justifyContent: "center",
+  },
+  justifyEnd: {
+    justifyContent: "flex-end",
+  },
+  justifyAround: {
+    justifyContent: "space-around",
+  },
+  justifyBetween: {
+    justifyContent: "space-between",
   },
 });
