6b88671b2dfeca5b057ef58c5c2e41c14b515aaf
Edward Ly <edvard.ly@gmail.com>
2018-01-25 12:35:42 -0800

    Added some text style classes

--- src/js/styles/TextStyles.js
+++ src/js/styles/TextStyles.js
@@ -1,9 +1,24 @@
 import { StyleSheet } from "react-native";
 
 export default TextStyles = StyleSheet.create({
+  // font-size
+  size8: {
+    fontSize: 8,
+  },
+  size16: {
+    fontSize: 16,
+  },
   size32: {
     fontSize: 32,
   },
+  // color
+  green: {
+    color: "green",
+  },
+  red: {
+    color: "red",
+  },
+  // text-align
   alignCenter: {
     textAlign: "center",
   },
