65c1923f89c5e918bc7eac0cc66dd7735fadd209
Edward Ly <edvard.ly@gmail.com>
2018-01-25 12:35:07 -0800

    Added textStyles property

--- src/js/components/answer/StatusMessage.js
+++ src/js/components/answer/StatusMessage.js
@@ -8,13 +8,18 @@ import TextStyles from "../../styles/TextStyles";
 export default class StatusMessage extends Component {
   static propTypes = {
     message: PropTypes.string,
+    textStyles: PropTypes.array,
   }
   
   render () {
     let message = this.props.message || "";
+    let styles = this.props.textStyles || [];
+    let text_styles = [TextStyles.size32, TextStyles.alignCenter];
+    text_styles.push(styles);
+
     return (
       <View style={[FlexStyles.flex, FlexStyles.alignCenter, FlexStyles.justifyCenter]}>
-        <Text style={[TextStyles.size32, TextStyles.alignCenter]}>{message}</Text>
+        <Text style={text_styles}>{message}</Text>
       </View>
     );
   }
