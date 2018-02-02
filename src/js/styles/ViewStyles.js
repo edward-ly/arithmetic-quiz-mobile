1002d7ff66aa77afbf1fb47c8be75ff577c1b47e
Edward Ly <edvard.ly@gmail.com>
2018-02-01 16:52:50 -0800

    Added ViewStyles stylesheet

---
+++ src/js/styles/ViewStyles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default ViewStyles = StyleSheet.create({
+  mainPageContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 24,
+    paddingRight: 12,
+    paddingLeft: 12,
+  },
+});
