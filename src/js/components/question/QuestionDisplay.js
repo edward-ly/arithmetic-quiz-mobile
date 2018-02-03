20b9424c0c2204fbe93f0ea3698a132a0a21d085
Edward Ly <edvard.ly@gmail.com>
2018-02-02 22:32:26 -0800

    Updated properties and render function to show hint colors

--- src/js/components/question/QuestionDisplay.js
+++ src/js/components/question/QuestionDisplay.js
@@ -7,14 +7,29 @@ import TextStyles from "../../styles/TextStyles";
 
 export default class QuestionDisplay extends Component {
   static propTypes = {
-    question: PropTypes.string,
+    question: PropTypes.array.isRequired,
+    hint: PropTypes.array.isRequired,
+    showHint: PropTypes.bool.isRequired,
   }
   
   render () {
-    let question = this.props.question || "";
+    const hint_colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+    let { question, hint, showHint } = this.props;
+    let question_display = question.map((item, i) => {
+      return (
+        <Text key={i}
+              style={{ fontSize: 32,
+                       textAlign: "center",
+                       margin: 6,
+                       color: showHint ? hint_colors[hint[i]] : "black" }}>
+          { item }
+        </Text>
+      );
+    });
+
     return (
-      <View style={[FlexStyles.flex, FlexStyles.alignCenter, FlexStyles.justifyCenter]}>
-        <Text style={[TextStyles.size32, TextStyles.alignCenter]}>{question}</Text>
+      <View style={[FlexStyles.flex, FlexStyles.flexRow, FlexStyles.alignCenter, FlexStyles.justifyCenter]}>
+        { question_display }
       </View>
     );
   }
