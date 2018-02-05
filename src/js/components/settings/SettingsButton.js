6be8c241b9ea2d3833b55b0e6d4ebcc2dceba842
Edward Ly <edvard.ly@gmail.com>
2018-02-04 20:55:05 -0800

    Added SettingsButton component

---
+++ src/js/components/settings/SettingsButton.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Button, View } from "react-native";
+
+import FlexSpace from "../layout/FlexSpace";
+
+import FlexStyles from "../../styles/FlexStyles";
+
+export default class SettingsButton extends Component {
+  static propTypes = {
+    onPress: PropTypes.func.isRequired,
+  }
+  
+  render () {
+    return (
+      <View style={FlexStyles.flex}>
+        <View style={[FlexStyles.flex, FlexStyles.flexRow, FlexStyles.alignCenter, FlexStyles.justifyCenter]}>
+          <FlexSpace flex={3} />
+          <View style={FlexStyles.flex}>
+            <Button onPress={this.props.onPress}
+                    title="Settings" />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
