abea43b620b26fc42a72e73a898d38ad31f2fb0b
Edward Ly <edvard.ly@gmail.com>
2018-02-04 20:05:22 -0800

    Added individual margin style classes

--- src/js/styles/TextStyles.js
+++ src/js/styles/TextStyles.js
@@ -38,4 +38,56 @@ export default TextStyles = StyleSheet.create({
   margin8: {
     margin: 8,
   },
+  // margin-top
+  marginTop2: {
+    marginTop: 2,
+  },
+  marginTop4: {
+    marginTop: 4,
+  },
+  marginTop8: {
+    marginTop: 8,
+  },
+  marginTop16: {
+    marginTop: 16,
+  },
+  // margin-right
+  marginRight2: {
+    marginRight: 2,
+  },
+  marginRight4: {
+    marginRight: 4,
+  },
+  marginRight8: {
+    marginRight: 8,
+  },
+  marginRight16: {
+    marginRight: 16,
+  },
+  // margin-bottom
+  marginBottom2: {
+    marginBottom: 2,
+  },
+  marginBottom4: {
+    marginBottom: 4,
+  },
+  marginBottom8: {
+    marginBottom: 8,
+  },
+  marginBottom16: {
+    marginBottom: 16,
+  },
+  // margin-left
+  marginLeft2: {
+    marginLeft: 2,
+  },
+  marginLeft4: {
+    marginLeft: 4,
+  },
+  marginLeft8: {
+    marginLeft: 8,
+  },
+  marginLeft16: {
+    marginLeft: 16,
+  },
 });
