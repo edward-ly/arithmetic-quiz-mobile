525a792fd22b0292b48ed9faf618ef7828d9ecea
Edward Ly <edvard.ly@gmail.com>
2018-02-10 16:49:43 -0800

    Removed flex declarations

--- src/js/styles/ViewStyles.js
+++ src/js/styles/ViewStyles.js
@@ -2,14 +2,12 @@ import { StyleSheet } from "react-native";
 
 export default ViewStyles = StyleSheet.create({
   mainPageContainer: {
-    flex: 1,
     backgroundColor: '#fff',
     paddingTop: 24,
     paddingRight: 12,
     paddingLeft: 12,
   },
   modalContainer: {
-    flex: 1,
     backgroundColor: '#fff',
     borderRadius: 8,
     padding: 16,
