e49fe37b1a0dd714729944735a9f875c862555a1
Edward Ly <edvard.ly@gmail.com>
2018-02-10 20:56:34 -0800

    Use double quotes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // Entry point for create-react-native-app
-import React from 'react';
-import Application from './src/js/Application';
+import React from "react";
+import Application from "./src/js/Application";
 
 export default function App () { return <Application />; }
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from './App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./App";
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
