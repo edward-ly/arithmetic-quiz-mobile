cc0d2700f8d6de376d5f691f39e601018c302656
Edward Ly <edvard.ly@gmail.com>
2018-02-10 16:19:20 -0800

    Refactored FlexSpace as sub-component of FlexView

--- src/js/components/layout/FlexSpace.js
+++ src/js/components/layout/FlexSpace.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
+
+import FlexView from "./FlexView";
 
 export default class FlexSpace extends Component {
   static propTypes = {
     flex: PropTypes.number,
+    styles: PropTypes.number, // style object disguised as int
   }
   
   render () {
-    let flex = this.props.flex || 1;
-    return <View style={{ flex: flex }} />;
+    let { flex, styles } = this.props;
+    return <FlexView flex={flex} styles={styles} />;
   }
 }
