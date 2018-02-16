f5a0c4ed001b9e94702940dcb2931da1092cadc0
Edward Ly <edvard.ly@gmail.com>
2018-02-16 11:46:57 -0800

    Moved drawer toggle button styles to ButtonStyles

--- src/js/components/navigation/Header.js
+++ src/js/components/navigation/Header.js
@@ -6,6 +6,7 @@ import Icon from "react-native-vector-icons/FontAwesome";
 import FlexSpace from "../layout/FlexSpace";
 import FlexView from "../layout/FlexView";
 
+import ButtonStyles from "../../styles/ButtonStyles";
 import FlexStyles from "../../styles/FlexStyles";
 
 export default class Header extends Component {
@@ -17,7 +18,7 @@ export default class Header extends Component {
     return (
       <FlexView styles={[FlexStyles.flexRow, FlexStyles.alignCenter, FlexStyles.justifyBetween]}>
         <FlexSpace />
-        <TouchableOpacity style={{ alignItems: "center", backgroundColor: "transparent", borderColor: "#666", borderWidth: 2, borderRadius: 4, width: 40, }}
+        <TouchableOpacity style={ButtonStyles.drawerToggleButton}
                           onPress={this.props.onPress}>
           <Icon name="bars" size={32} color="black" />
         </TouchableOpacity>
