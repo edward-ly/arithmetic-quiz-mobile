ac5e80d3fbf9a6de019ef19e2ef3b1ff240222a1
Edward Ly <edvard.ly@gmail.com>
2018-02-15 21:39:27 -0800

    Imported global variables

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 // Entry point for create-react-native-app
 import React from "react";
+import Variables from "./src/js/global/Variables";
 import Application from "./src/js/Application";
 
 export default function App () { return <Application />; }
