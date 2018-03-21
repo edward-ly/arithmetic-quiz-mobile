772fc6d365f238bab1105cee81711c22c505850c
Edward Ly <edvard.ly@gmail.com>
2018-03-20 23:18:44 -0700

    Refactored function declarations

--- src/js/utilities/Platform.js
+++ src/js/utilities/Platform.js
@@ -3,32 +3,32 @@ import { Dimensions } from "react-native";
 
 /** @param {ScaledSize} dim the dimensions object
  ** @param {*} limit the limit on the scaled dimension */
-const msp = (dim, limit) => {
+function msp (dim, limit) {
   return dim.scale * dim.width >= limit || dim.scale * dim.height >= limit;
-};
+}
 
 // Returns true if the screen is in portrait mode
-const isPortrait = () => {
+function isPortrait () {
   const dim = Dimensions.get("screen");
   return dim.height >= dim.width;
-};
+}
 
 // Returns true if the screen is in landscape mode
-const isLandscape = () => {
+function isLandscape () {
   const dim = Dimensions.get("screen");
   return dim.width >= dim.height;
-};
+}
 
 // Returns true if the device is a tablet
-const isTablet = () => {
+function isTablet () {
   const dim = Dimensions.get("screen");
   return dim.scale < 2 && msp(dim, 1000) || dim.scale >= 2 && msp(dim, 1900);
-};
+}
 
 // Returns true if the device is a phone
-const isPhone = () => {
+function isPhone () {
   return !isTablet();
-};
+}
 
 module.exports = {
   isPortrait,
